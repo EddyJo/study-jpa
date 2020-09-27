75789a36684aa416d6dbe9d56023293ffd204a79
EddyJo <jkjk0600@gamil.com>
2020-09-27 20:06:11 +0900

    chore (java) : 안쓰는 import 제거

--- src/main/java/kr/co/eddy/study/jpa/studyjpa/fruit/repository/FruitRepository.java
+++ src/main/java/kr/co/eddy/study/jpa/studyjpa/fruit/repository/FruitRepository.java
@@ -1,15 +1,9 @@
 package kr.co.eddy.study.jpa.studyjpa.fruit.repository;
 
 import kr.co.eddy.study.jpa.studyjpa.fruit.domain.Fruit;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
